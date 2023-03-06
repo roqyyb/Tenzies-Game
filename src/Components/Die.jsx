ec38ae191896b6a6624590b34251326dc98ab141
0latunde1 <rooladokun@student.oauife.edu.ng>
2023-03-06 09:34:33 +0100

    Removed die-container from the Die component

--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -2,8 +2,6 @@ import DieDots from "./DieDots"
 
 export default function Die(){
     return(
-        <div className="die-container">
-             <DieDots/>
-        </div>
+        <DieDots/>
     )
 }
