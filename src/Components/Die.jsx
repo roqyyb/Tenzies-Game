79c74cc6d066ffad47ef9ccd3572f7cb61fac621
0latunde1 <rooladokun@student.oauife.edu.ng>
2023-03-06 09:53:49 +0100

    Updated the Die component

--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -9,6 +9,10 @@ export default function Die(){
         <DieDots />
         <DieDots />
         <DieDots />
+        <DieDots />
+        <DieDots />
+        <DieDots />
+        <DieDots />
         </>
     )
 }
